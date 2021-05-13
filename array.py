e25375d8b6874faf4ede76f6f65105eeb2b6ab61
SperZ <ZachSperka@gmail.com>
2021-05-13 11:39:37 -0400

    first commit

---
+++ array.py
@@ -0,0 +1,18 @@
+arr1 = [1,2,3,4,5]
+arr2 = [4,5,6,7,8]
+
+# combines to arrays together to create one array with all elements of both arrays
+arr3 = arr1 + arr2;
+print(arr3);
+
+# 
+
+arr_repeat = [];
+
+arr_repeat.append("eat");
+arr_repeat.append("sleep");
+arr_repeat.append("repeat");
+
+arr_repeat.pop();# removes the item from the array/list at the give index 
+print(arr_repeat);
+#print(popped);
---
+++ first.py
@@ -0,0 +1,44 @@
+
+# initialize variable and assign value
+x = 7;
+x +=5;
+
+x**5; # raise the number to the power of something.
+
+# initialize array and value, 
+letters = ["a","b","c","d","e"]
+
+# python arrays can contain elements of many different variable types
+a = "3.9"
+randomArr = [1,"python", 2.46, True, a]
+
+print(letters);
+print(randomArr);
+
+# string manipulation
+python = "python";
+python += " Hello world";
+python[1] # result = y this looks for the element in the second index of the string 
+print(python);
+print(python[-1]); # result d -1 goes to the last element in the array
+print(python.capitalize()) # capitalizes the first element of the string and if any other element is capitalized it changes them to lowercase
+python.upper() # makes all letters in string UPPERCASE
+python.lower(); # makes all letters in string lower case
+print(python.__len__()); # returns the length of the string
+str1 = "hello World";
+print("Hello World " * 3); # will print hello world three times
+
+# python math increment and decrement in place
+int_a = 5;
+int_a -=1; # result int_a = 4
+int_a +=1; # result int_a = 5
+int_a *=2 # result int_a = 10;
+int_a **=2 # result int_a =100
+int_a /=10; # result int_a = 10
+
+print(int_a);
+
+
+
+
+
